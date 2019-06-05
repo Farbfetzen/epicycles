784f59a46693a8354d2fce2eff0d92aa9db9015f
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-06-05 17:06:29 +0200

    Separate most of the epicycle logic from app logic
    closes #19

--- epicycles.py
+++ epicycles.py
@@ -1,7 +1,4 @@
-"""Draw various intricate shapes by adding rotating circles.
-
-
-Copyright (C) 2019 Sebastian Henz
+"""Copyright (C) 2019 Sebastian Henz
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,22 +15,19 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
 
-import os
 import math
 from numpy.fft import ifft
-import argparse
 
-# os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame as pg
 
 
 # TODO: Load harmonics from file if given a filename. Modify the file format
-# output by the R script, i.e. remove the brackets and trailing commas.
+#       output by the R script, i.e. remove the brackets and trailing commas.
 
 # aalines don't look right. They always have some gaps in between them.
 # So instead of using aalines I draw non-aalines but onto a big surface
 # which gets smoothscaled down into the window. This produces a nice looking
-# result, is fast and easy.
+# result in a fast and easy way.
 
 # This is the formula:
 # a * exp(bj * t) + c
@@ -42,55 +36,32 @@ import pygame as pg
 # j is sqrt(-1), usually denoted "i" in math and physics
 # c is the position of the circle center
 
-DEFAULT_WINDOW_WIDTH = 700
-DEFAULT_WINDOW_HEIGHT = 700
-SMOOTH_SCALE_FACTOR = 1.5  # < 2 for performance but > 1 nice looking result
-FPS = 30
-BACKGROUND_COLOR = (255, 255, 255)
 PATH_COLOR = (255, 0, 0)
+BACKGROUND_COLOR = (255, 255, 255)
 CIRCLE_COLOR = (170, 170, 170)
 CIRCLE_LINE_COLOR = (60, 60, 60)
-MIN_SPEED = 1/16
-MAX_SPEED = 4
-DEFAULT_SCALE_FACTOR = 0.8
 CIRCLE_LINE_THICKNESS = 1
 PATH_LINE_THICKNESS = 3
+SMOOTH_SCALE_FACTOR = 1.5  # < 2 for performance but > 1 nice looking result
 # Max. distance between two points before interpolation kicks in:
 MAX_DIST = 5 * SMOOTH_SCALE_FACTOR
+MIN_SPEED = 1/16
+MAX_SPEED = 4
 
 
 class Epicycles:
-    def __init__(self, points_file, n, scale_factor, fade,
-                 reverse_rotation, start_paused):
-        self.running = True
+    def __init__(self, window_width, window_height, points_file, n,
+                 scale_factor, fade, reverse_rotation):
         self.speed = 1  # speed of the innermost circle in radians/second
-        self.paused = start_paused
         self.circles_visible = True
         self.fade = fade
         self.angle = 0  # angle in radians
         self.previous_angle = self.angle
         self.angle_increment = 0
-        # Create clock instance in self.run() so time doesn't run during setup.
-        self.clock = None
 
-        # Setup surfaces:
-        # TODO: Make the default window size adjustable via command line arguments
-        self.window_width = DEFAULT_WINDOW_WIDTH
-        self.window_height = DEFAULT_WINDOW_HEIGHT
-        display_info = pg.display.Info()
-        display_width = display_info.current_w
-        display_height = display_info.current_h
-        if DEFAULT_WINDOW_WIDTH >= display_width:
-            self.window_width = display_width - 200
-            self.window_height = self.window_width
-        if DEFAULT_WINDOW_HEIGHT >= display_height:
-            self.window_height = display_height - 200
-            self.window_width = self.window_height
+        self.window_width = window_width
+        self.window_height = window_height
 
-        self.window = pg.display.set_mode(
-            (self.window_width, self.window_height)
-        )
-        pg.display.set_caption("Epicycles")
         self.big_surface = pg.Surface((
             self.window_width * SMOOTH_SCALE_FACTOR,
             self.window_height * SMOOTH_SCALE_FACTOR
@@ -128,8 +99,8 @@ class Epicycles:
         # Setup circles and points:
         self.circle_points = [0j] * (len(self.harmonics) + 1)
         self.circle_points[0] = self.to_complex((
-            self.window_width // 2 * SMOOTH_SCALE_FACTOR - int(offset[0]),
-            self.window_height // 2 * SMOOTH_SCALE_FACTOR - int(offset[1])
+            window_width // 2 * SMOOTH_SCALE_FACTOR - int(offset[0]),
+            window_height // 2 * SMOOTH_SCALE_FACTOR - int(offset[1])
         ))
         self.circle_radii = []
         for h in self.harmonics:
@@ -140,126 +111,8 @@ class Epicycles:
         self.point = self.get_new_point(self.angle)
         self.interpolated_points = ()
 
-    @staticmethod
-    def to_complex(xy):
-        return complex(xy[0], xy[1])
-
-    @staticmethod
-    def from_complex(z):
-        return z.real, z.imag
-
-    @staticmethod
-    def get_dist(p1, p2):
-        return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
-
-    def load_path(self, points_file, scale_factor):
-        all_x = []
-        all_y = []
-        with open(points_file, "r") as file:
-            for line in file:
-                x, y = line.split()
-                all_x.append(float(x))
-                all_y.append(float(y))
-
-        if scale_factor != 0:
-            if 0 < scale_factor <= 1:
-                max_allowed_x = self.window_width / 2 * scale_factor
-                max_allowed_y = self.window_height / 2 * scale_factor
-                if max_allowed_x <= max_allowed_y:
-                    ratio = max_allowed_x / max(map(abs, all_x))
-                    all_x = [x * ratio for x in all_x]
-                    all_y = [y * ratio for y in all_y]
-                else:
-                    ratio = max_allowed_y / max(map(abs, all_y))
-                    all_x = [x * ratio for x in all_x]
-                    all_y = [y * ratio for y in all_y]
-            else:
-                raise ValueError(
-                    "Argument 'scale_factor' must be between 0 and 1."
-                )
-            all_x = [x * SMOOTH_SCALE_FACTOR for x in all_x]
-            all_y = [y * SMOOTH_SCALE_FACTOR for y in all_y]
-
-        return [complex(*i) for i in zip(all_x, all_y)]
-
-    def transform(self, path, reverse=False):
-        transformed = ifft(path)
-        transformed = list(transformed)
-        offset = self.from_complex(transformed.pop(0))
-        h = []
-        i = 1
-        increase_i = False
-        sign = 1 if reverse else -1
-        pop_back = True  # pop from the front or the back
-        while transformed:
-            radius = transformed.pop(-pop_back)
-            if abs(radius) >= 0.1:
-                h.append([radius, complex(0, sign * i)])
-            if increase_i:
-                i += 1
-            increase_i = not increase_i
-            sign *= -1
-            pop_back = not pop_back
-        return h, offset
-
-    def handle_input(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                self.running = False
-            elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_ESCAPE:
-                    self.running = False
-                elif event.key == pg.K_p or event.key == pg.K_SPACE:
-                    self.paused = not self.paused
-                elif event.key == pg.K_c:
-                    self.circles_visible = not self.circles_visible
-                elif event.key in (pg.K_PLUS, pg.K_KP_PLUS):
-                    self.speed = min(self.speed * 2, MAX_SPEED)
-                elif event.key in (pg.K_MINUS, pg.K_KP_MINUS):
-                    self.speed = max(self.speed / 2, MIN_SPEED)
-                elif event.key == pg.K_BACKSPACE:
-                    self.line_surface.fill(BACKGROUND_COLOR)
-                elif event.key == pg.K_d:
-                    debug_info = (
-                        f"\nangle: {round(self.angle, 3)} radians" +
-                        f" ({round(math.degrees(self.angle), 2)}°)" +
-                        f"\nspeed: {self.speed}" +
-                        f"\nfps: {int(self.clock.get_fps())}"
-                    )
-                    print(debug_info)
-
-    def get_new_point(self, angle):
-        for i, h in enumerate(self.harmonics):
-            p = h[0] * math.e ** (h[1] * angle) + self.circle_points[i]
-            self.circle_points[i+1] = p
-        return self.from_complex(self.circle_points[-1])
-
-    def update(self, dt):
-        self.previous_angle = self.angle
-        self.angle_increment = self.speed * dt
-        self.angle += self.angle_increment
-        self.previous_point = self.point
-        self.point = self.get_new_point(self.angle)
-
-        if self.get_dist(self.previous_point, self.point) > MAX_DIST:
-            self.interpolated_points = (
-                    (self.previous_point,) +
-                    self.interpolate(
-                        self.previous_point,
-                        self.point,
-                        self.previous_angle,
-                        self.angle
-                    ) +
-                    (self.point,)
-            )
-        else:
-            self.interpolated_points = ()
-
-        if self.angle > math.tau:
-            self.angle -= math.tau
-
-    def draw(self):
-        if not self.paused:
+    def draw(self, target_surf, paused):
+        if not paused:
             if self.fade:
                 self.alpha_angle += self.angle_increment
                 while self.alpha_angle > self.angle_per_alpha:
@@ -308,8 +161,25 @@ class Epicycles:
         pg.transform.smoothscale(
             self.big_surface,
             (self.window_width, self.window_height),
-            self.window
+            target_surf
         )
+
+    def erase_line(self):
+        self.line_surface.fill(BACKGROUND_COLOR)
+
+    @staticmethod
+    def from_complex(z):
+        return z.real, z.imag
+
+    @staticmethod
+    def get_dist(p1, p2):
+        return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
+
+    def get_new_point(self, angle):
+        for i, h in enumerate(self.harmonics):
+            p = h[0] * math.e ** (h[1] * angle) + self.circle_points[i]
+            self.circle_points[i + 1] = p
+        return self.from_complex(self.circle_points[-1])
 
     def interpolate(self, p1, p2, a1, a2):
         mean_angle = (a1 + a2) / 2
@@ -322,68 +192,86 @@ class Epicycles:
             result += self.interpolate(new_point, p2, mean_angle, a2)
         return result
 
-    def run(self):
-        self.clock = pg.time.Clock()
-        while self.running:
-            dt = self.clock.tick(FPS) / 1000  # seconds
-            self.handle_input()
-            if not self.paused:
-                self.update(dt)
-            self.draw()
-            pg.display.update()
+    def load_path(self, points_file, scale_factor):
+        all_x = []
+        all_y = []
+        with open(points_file, "r") as file:
+            for line in file:
+                x, y = line.split()
+                all_x.append(float(x))
+                all_y.append(float(y))
 
+        if scale_factor != 0:
+            if 0 < scale_factor <= 1:
+                max_allowed_x = self.window_width / 2 * scale_factor
+                max_allowed_y = self.window_height / 2 * scale_factor
+                if max_allowed_x <= max_allowed_y:
+                    ratio = max_allowed_x / max(map(abs, all_x))
+                    all_x = [x * ratio for x in all_x]
+                    all_y = [y * ratio for y in all_y]
+                else:
+                    ratio = max_allowed_y / max(map(abs, all_y))
+                    all_x = [x * ratio for x in all_x]
+                    all_y = [y * ratio for y in all_y]
+            else:
+                raise ValueError(
+                    "Argument 'scale_factor' must be between 0 and 1."
+                )
+            all_x = [x * SMOOTH_SCALE_FACTOR for x in all_x]
+            all_y = [y * SMOOTH_SCALE_FACTOR for y in all_y]
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "file",
-        help="Path to file containing the points of the shape."
-    )
-    parser.add_argument(
-        "-n",
-        type=int,
-        help="Maximum number of harmonics or circles.",
-        metavar="",
-        default = None
-    )
-    parser.add_argument(
-        "-s",
-        "--scale_factor",
-        type=float,
-        metavar="",
-        help="A number > 0 and <= 1 indicating how much of the width and " +
-             "height of the window the shape should occupy. To disable " +
-             f"rescaling set it to 0. Defaults to {DEFAULT_SCALE_FACTOR}.",
-        default=DEFAULT_SCALE_FACTOR
-    )
-    parser.add_argument(
-        "-f",
-        "--fade",
-        action="store_true",
-        help="Fade the line over time so that it vanishes after one cycle."
-    )
-    parser.add_argument(
-        "-r",
-        "--reverse",
-        action="store_true",
-        help="Reverse the rotation direction of all circles."
-    )
-    parser.add_argument(
-        "-p",
-        "--paused",
-        action="store_true",
-        help="Start the app paused."
-    )
-    args = parser.parse_args()
+        return [complex(*xy) for xy in zip(all_x, all_y)]
 
-    os.environ["SDL_VIDEO_CENTERED"] = "1"
-    pg.init()
-    E = Epicycles(
-        args.file,
-        args.n,
-        args.scale_factor,
-        args.fade,
-        args.reverse,
-        args.paused
-    )
-    E.run()
+    def speed_down(self):
+        self.speed = max(self.speed / 2, MIN_SPEED)
+
+    def speed_up(self):
+        self.speed = min(self.speed * 2, MAX_SPEED)
+
+    @staticmethod
+    def to_complex(xy):
+        return complex(xy[0], xy[1])
+
+    def transform(self, path, reverse=False):
+        transformed = ifft(path)
+        transformed = list(transformed)
+        offset = self.from_complex(transformed.pop(0))
+        h = []
+        i = 1
+        increase_i = False
+        sign = 1 if reverse else -1
+        pop_back = True  # pop from the front or the back
+        while transformed:
+            radius = transformed.pop(-pop_back)
+            if abs(radius) >= 0.1:
+                h.append([radius, complex(0, sign * i)])
+            if increase_i:
+                i += 1
+            increase_i = not increase_i
+            sign *= -1
+            pop_back = not pop_back
+        return h, offset
+
+    def update(self, dt):
+        self.previous_angle = self.angle
+        self.angle_increment = self.speed * dt
+        self.angle += self.angle_increment
+        self.previous_point = self.point
+        self.point = self.get_new_point(self.angle)
+
+        if self.get_dist(self.previous_point, self.point) > MAX_DIST:
+            self.interpolated_points = (
+                    (self.previous_point,) +
+                    self.interpolate(
+                        self.previous_point,
+                        self.point,
+                        self.previous_angle,
+                        self.angle
+                    ) +
+                    (self.point,)
+            )
+        else:
+            self.interpolated_points = ()
+
+        if self.angle > math.tau:
+            self.angle -= math.tau
